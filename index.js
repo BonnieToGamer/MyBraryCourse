d27a51b1a5751255205311ad0bd389fd0d9d2984
BonnieToGamer <BonnieToGamer@lemons.se>
2021-11-12 21:20:26 +0100

    Added files

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import njk from 'nunjucks';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import Mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+import router from './routes/index.js';
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
+
+// __dirname and __filename fix for ECMAScript module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+njk.configure(
+  "views", {
+    express: app
+  }
+);
+
+app.set('view engine', 'njk');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(express.static('public'));
+
+Mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+const db = Mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log("Connected to Mongoose"));
+
+app.use('/', router);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening to port ${process.env.PORT || 3000}. http://localhost:${process.env.PORT, 3000}`)
+});
